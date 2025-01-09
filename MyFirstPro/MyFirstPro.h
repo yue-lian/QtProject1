d489faf7a33692cfde86a06a2c2360b157f9d43c
森蕾赵 <1723705023@qq.com>
2025-01-09 23:15:57 +0800

    还原测试

--- MyFirstPro/MyFirstPro.h
+++ MyFirstPro/MyFirstPro.h
@@ -19,34 +19,6 @@ private:
     Ui::MyFirstProClass ui;
 };
 
-
-/*
-定义场景：下课了，老师跟同学说肚子饿了（信号），学生请老师吃饭（槽）
-首先定义一个学生类和老师类：
-老师类中声明信号 饿了 hungry
-*/
-class Tescher:public QObject
-{
-    Q_OBJECT
-
-public:
-
-signals:
-    void hungry();
-public:
-
-};
-
-class Student : public QWidget
-{
-    Q_OBJECT
-    //学生类 声明 槽
-public slots:
-    void treat();
-private:
-
-};
-
 #endif // !_OI_MYFIRSTPRO_MYFIRSTPRO_H
 
 
